8a7c13c81f7736744332c007061fdf7f3a695416
Emelon55 <emilis.sku@gmail.com>
2025-02-26 19:47:13 +0200

    main menu
    main menu

---
+++ scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Button startNewGame = GetNode<Button>("VBoxContainer/Button");
+		Button continueGame = GetNode<Button>("VBoxContainer/Continue");
+		Button options = GetNode<Button>("VBoxContainer/Options");
+		Button exit = GetNode<Button>("VBoxContainer/Exit");
+		
+		startNewGame.Pressed += OnStartNewGamePressed;
+        continueGame.Pressed += OnContinuePressed;
+        options.Pressed += OnOptionsPressed;
+        exit.Pressed += OnExitPressed;
+	}
+    
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+	{
+		
+	}
+	
+	private void OnStartNewGamePressed()
+	{
+		GD.Print("Start Game Button Pressed");
+	}
+    private void OnContinuePressed()
+    {
+        GD.Print("Continue  Button Pressed");
+    }
+    private void OnOptionsPressed()
+    {
+        GD.Print("Options Button Pressed");
+    }
+    private void OnExitPressed()
+	{
+		GD.Print("Exit Game Button Pressed");
+		GetTree().Quit();
+	}
+}
