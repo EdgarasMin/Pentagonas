fe75dc6b9ac8b700ff593a8642604e12a3bf34d7
Elvness <benediktas.simas999@gmail.com>
2025-03-05 23:14:23 +0200

    Bug fix

--- scripts/Global.cs
+++ scripts/Global.cs
@@ -4,4 +4,7 @@ using System;
 public partial class Global : Node
 {
 	public static string LastScene = "";
+	public static int VolumeSliderValue = 80;
+	public static int VolumeSFXSliderValue = 80;
+	public static bool MuteToggled = false;
 }
--- scripts/Nustatymai.cs
+++ scripts/Nustatymai.cs
@@ -3,24 +3,60 @@ using System;
 
 public partial class Nustatymai : Control
 {
+
+	public HSlider sliderMusic;
+	public HSlider sliderSFX;
+	public CheckBox muteButton;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		VBoxContainer vbox = GetNode<VBoxContainer>("VBoxContainer");
+
+		sliderMusic = vbox.GetNode<HSlider>("Volume");
+		sliderMusic.Value = Global.VolumeSliderValue;
+		
+		sliderSFX = vbox.GetNode<HSlider>("VolumeSFX");
+		sliderSFX.Value = Global.VolumeSFXSliderValue;
+
+		muteButton = vbox.GetNode<CheckBox>("Mute");
+		
+
 	}
+
+	int SFX_id = AudioServer.GetBusIndex("SFX");
+	int Music_id = AudioServer.GetBusIndex("Music");
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
+		
 	}
 	
 	public void _on_volume_value_changed(float x)
 	{
-		AudioServer.SetBusVolumeDb(0, x);
+		
+		AudioServer.SetBusVolumeDb(Music_id, x-80);
+		//AudioServer.SetBusMute(Music_id, x<0.5);
+		Global.VolumeSliderValue = (int)x;
+		
+	}
+	
+	public void _on_volume_sfx_value_changed(float x)
+	{
+		
+		AudioServer.SetBusVolumeDb(SFX_id, x-80);
+		//AudioServer.SetBusMute(Music_id, x<0.5);
+		Global.VolumeSFXSliderValue = (int)x;
+		
 	}
 	
 	public void _on_mute_toggled(bool toggled)
 	{
-		AudioServer.SetBusMute(0,toggled);
+		//int Music_id = AudioServer.GetBusIndex("Music");
+		AudioServer.SetBusMute(Music_id,toggled);
+		Global.MuteToggled = toggled;
 	}
 	
 	public void _on_resolutions_item_selected(int item)
