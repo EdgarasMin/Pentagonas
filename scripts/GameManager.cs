07e7fb914c00f16c273faca8e1e6ac7c9fe0f94f
Emelon55 <emilis.sku@gmail.com>
2025-04-12 01:21:43 +0300

    ss

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -1,15 +1,38 @@
+// GameManager.cs
 using Godot;
 using System;
 
 public partial class GameManager : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    public static GameManager Instance { get; private set; }
+    public int CurrentLevel { get; private set; } = 1;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Ready()
+    {
+        if (Instance != null)
+        {
+            QueueFree();
+            return;
+        }
+
+        Instance = this;
+       // CurrentLevel = SaveSystem.LoadProgress();
+    }
+
+    public void CompleteCurrentLevel()
+    {
+        CurrentLevel = Math.Clamp(CurrentLevel + 1, 1, 3); // Assuming 3 levels
+        SaveSystem.SaveProgress(CurrentLevel);
+    }
+
+    public void LoadCurrentLevel()
+    {
+        GetTree().ChangeSceneToFile($"res://Levels/Level{CurrentLevel}.tscn");
+    }
+
+    public void ResetProgress()
+    {
+        CurrentLevel = 1;
+        SaveSystem.ResetProgress();
+    }
 }
